f7afbbf2298537e8527a18918d45bc5cebe67d17
stscoundrel <sampo@pixels.fi>
2020-10-10 14:16:05 +0300

    Add swedish alphabet & test cases

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const ELFDALIAN = require('./constants/elfdalian.js')
 const FAROESE = require('./constants/faroese.js')
 const ICELANDIC = require('./constants/icelandic.js')
+const SWEDISH = require('./constants/swedish.js')
 
 module.exports = {
   ELFDALIAN,
   FAROESE,
   ICELANDIC,
+  SWEDISH,
 }
---
+++ tests/swedish.test.js
@@ -0,0 +1,49 @@
+const { SWEDISH } = require('../index.js')
+
+const { ALPHABET, LOWER_CASE, UPPER_CASE } = SWEDISH
+
+describe('Swedish alphabet', () => {
+  describe('Lowercase constants', () => {
+    test('Alphabet is returned in array format', () => {
+      expect(Array.isArray(LOWER_CASE)).toBeTruthy()
+    })
+
+    test('Alphabet contains 29 characters', () => {
+      expect(LOWER_CASE.length).toBe(29)
+    })
+
+    test('Characters are returned in lowercase.', () => {
+      LOWER_CASE.forEach((letter) => (
+        expect(letter).toBe(letter.toLowerCase())
+      ))
+    })
+
+    test('All characters also exist in main alphabet.', () => {
+      LOWER_CASE.forEach((letter) => (
+        expect(ALPHABET.includes(letter)).toBeTruthy()
+      ))
+    })
+  })
+
+  describe('Uppecase constants', () => {
+    test('Alphabet is returned in array format', () => {
+      expect(Array.isArray(UPPER_CASE)).toBeTruthy()
+    })
+
+    test('Alphabet contains 29 characters', () => {
+      expect(UPPER_CASE.length).toBe(29)
+    })
+
+    test('Characters are returned in uppercase.', () => {
+      UPPER_CASE.forEach((letter) => (
+        expect(letter).toBe(letter.toUpperCase())
+      ))
+    })
+
+    test('All characters also exist in main alphabet.', () => {
+      UPPER_CASE.forEach((letter) => (
+        expect(ALPHABET.includes(letter)).toBeTruthy()
+      ))
+    })
+  })
+})
